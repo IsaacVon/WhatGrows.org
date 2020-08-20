23b4e9134aeb9d2b0296c274c82069ead1660a80
Isaac Householder <isaachouseholder@gmail.com>
2020-08-20 14:14:05 -0700

    mongoDb server all functions working

--- server/mongoDbServer.js
+++ server/mongoDbServer.js
@@ -6,7 +6,6 @@ mongoose
     useNewUrlParser: true,
     useFindAndModify: false,
   })
-
   .then(() => console.log("connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
 
@@ -27,27 +26,25 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 // GET
-
-  // DONE
 const getUser = async (id) => {
-  return await User.find({ _id: id });
+  const user = await User.find({ _id: id });
+  console.log("getUser:", user);
+  return user;
 };
 
 // CRUD
-
-  // DONE
 const createUser = async (email, password) => {
   const user = new User({
     email,
     password,
   });
-
-  return await user.save();
+  const newUser = await user.save();
+  console.log("newUser:", newUser);
+  return newUser;
 };
-  // DONE
-async function addFavorite(id, plantObject) {
+const addFavorite = async (id, plantObject) => {
   // take in plantObject and map it down there
-  return await User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     id,
     {
       $push: {
@@ -56,60 +53,50 @@ async function addFavorite(id, plantObject) {
     },
     { new: true }
   );
-}
 
-  // DONE
-async function deleteAFavorite(id, plantMongoId) {
-  // take in plantObject and map it down there
-  const user =  await User.findOneAndUpdate(
+  console.log("addFavorites, all favorites:", user);
+  return user;
+};
+const deleteAFavorite = async (id, plantMongoId) => {
+  const user = await User.findOneAndUpdate(
     { _id: id },
     { $pull: { favorites: { _id: plantMongoId } } },
     { new: true }
-  )
-}
+  );
+  console.log("deleteAFavorite, user:", user);
+  return user;
+};
+const deleteAllFavorites = async (id) => {
+  const user = await User.findOneAndUpdate(
+    { _id: id },
+    { $set: { favorites: [] } },
+    { new: true }
+  );
+  console.log("deleteAllFavorites, user:", user);
 
+  return user;
+};
 
-
-async function deleteAllFavorites(user) {
-  const result = await Favorite.deleteMany({ user });
-  console.log(result);
-}
-
-// createUser()
-// addFavorite("5f3d87329bd0717c5f42bb8f")
-// getFavorites("5f3c79c395044c6ddbc8f96c")
-
-// const userID = "5f3dbfd0737f428b535b68f9";
+// Add favorite to a user
+// const userID = "5f3edddf57054fa48f0dd4fd";
 // const plantObject = {
-//   plantId: "1111111",
+//   plantId: "9999999",
 //   common_name: "FAVORITES WORKING",
 //   notes: "Plant in front yard",
 //   image: "http//fdasffadsfads",
 //   plantUrl: "http//fdasffadsfads",
 // };
-
-// async function run() {
-//   const user = await addFavorite(userID, plantObject);
-//   console.log(user);
-// }
-
-// run();
-
-
+// addFavorite(userID, plantObject);
 
 // Create 4 new users
-  // const email1 = "email1";
-  // const password1 = "password1";
-  // createUser(email1,password1)
+// const email2 = "email2"
+// const password2 = "password2"
+// createUser(email2,password2)
 
-  // const email2 = "email2"
-  // const password2 = "password2"
-  // createUser(email2,password2)
+// const email3 = "email3"
+// const password3 = "password3"
+// createUser(email3,password3)
 
-  // const email3 = "email3"
-  // const password3 = "password3"
-  // createUser(email3,password3)
-
-  // const email4 = "email4"
-  // const password4 = "password4"
-  // createUser(email4,password4)
+// const email4 = "email4"
+// const password4 = "password4"
+// createUser(email4,password4)
