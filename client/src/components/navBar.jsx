f1c6a5358ee8c78c0ac77529cac634987b43ecc8
Isaac Householder <isaachouseholder@gmail.com>
2020-11-19 15:15:29 -0800

    Adding Responsive layout to navbar

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -3,54 +3,88 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import logo from "../assets/logo.png";
 import styled from "styled-components";
+import { device } from "../utils/device";
 
-const Wrapper = styled.section`
-  color: white;
+const Parent = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  height: 250px;
+
+  @media ${device.mobileL} {
+    flex-direction: column;
+  }
+  @media ${device.tablet} {
+    flex-direction: row;
+  }
 `;
 
-const Logo = styled.section`
-  padding-top: 25px;
-  display: grid;
-  place-items: center;
+const LogoContainer = styled.section`
+  margin: 5px;
+  // background-color: yellowgreen;
+`;
+
+const Logo = styled.img`
+  left: 20px;
+  height: 111px;
+  width: 195px;
+  margin: 20px;
+
+  @media ${device.mobileL} {
+    position: relative;
+  }
+  @media ${device.tablet} {
+    position: absolute;
+  }
+`;
+
+const TextContainer = styled.section`
+  margin: 5px;
+  // display: grid;
+  // place-items: center;
+  // height: 50px;
 `;
 
 export default function NavBar({ name, loggedIn }) {
+  console.log("device", device.laptop);
   return (
     <>
-      <Logo>
-        <img src={logo} alt="WhatGrows" />
-      </Logo>
-
-      <Wrapper>
-        <Button component={Link} to="/">
-          Home
-        </Button>
-
-        <Button component={Link} to="/searchzip">
-          Search
-        </Button>
-
-        {loggedIn && (
-          <Button component={Link} to="/favorites">
-            Favorites
-          </Button>
-        )}
-        {loggedIn && (
-          <Button component={Link} to="/logout">
-            Sign Out
-          </Button>
-        )}
-        {!loggedIn && (
-          <Button component={Link} to="/signin">
-            Sign In
-          </Button>
-        )}
-        {!loggedIn && (
-          <Button component={Link} to="/signup">
-            Sign Up
-          </Button>
-        )}
-      </Wrapper>
+      <Parent>
+        <LogoContainer>
+          <Logo src={logo} />
+        </LogoContainer>
+        <TextContainer>
+          <div>
+            <Button component={Link} to="/">
+              Home
+            </Button>
+            <Button component={Link} to="/searchzip">
+              Search
+            </Button>
+            {loggedIn && (
+              <Button component={Link} to="/favorites">
+                Favorites
+              </Button>
+            )}
+            {loggedIn && (
+              <Button component={Link} to="/logout">
+                Sign Out
+              </Button>
+            )}
+            {!loggedIn && (
+              <Button component={Link} to="/signin">
+                Sign In
+              </Button>
+            )}
+            {!loggedIn && (
+              <Button component={Link} to="/signup">
+                Sign Up
+              </Button>
+            )}
+          </div>
+        </TextContainer>
+        <LogoContainer></LogoContainer>
+      </Parent>
     </>
   );
 }
